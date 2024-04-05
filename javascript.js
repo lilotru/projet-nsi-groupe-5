1de20a9c7ddb8932172239a913f30989edfab1fe
lilotru <sylvain.touitoubouchy@gmail.com>
2024-04-05 08:57:18 +0200

    javascript.js

--- javascript.js
+++ javascript.js
@@ -10,3 +10,43 @@ function entreePrenom()
     message = "Merci " + nom + ", votre réservation pour " + nombre_personnes + " personne(s) le " + date + " à " + heure + " a été confirmée.";
   document.getElementById("message").innerHTML = message;
 }
+
+
+var monbouton=document.getElementById("bouton");
+function maFonction()
+{
+    alert("en cours de procédure");
+}
+monbouton.onclick=maFonction;
+
+ var imagejavascript = document.createElement("img")
+
+ imagejavascript.src = "https://tse1.mm.bing.net/th?id=OIP.whrWTKNx-P5bH6ZTdrt9KgHaE8&pid=Api&P=0&h=180"
+ 
+var imagejavascript = document.createElement("img")
+
+ imagejavascript.src = "https://tse1.mm.bing.net/th?id=OIP.wO1cwBP-BCb6QK28NfaI2gHaHa&pid=Api&P=0&h=180"
+
+var imagejavascript = document.createElement("img")
+
+ imagejavascript.src = "https://tse4.mm.bing.net/th?id=OIP.6NBhir9iyW19YIlcXn8tmAHaD4&pid=Api&P=0&h=180"
+
+var imagejavascript = document.createElement("img")
+
+ imagejavascript.src = "https://tse3.mm.bing.net/th?id=OIP.mkwvc4IRQRXNvw24UA-mOAHaFj&pid=Api&P=0&h=180"
+
+var imagejavascript = document.createElement("img")
+
+ imagejavascript.src = "https://tse3.mm.bing.net/th?id=OIP.-HOmGCoCCk_agw7zfMvobgHaE7&pid=Api&P=0&h=180"
+
+var imagejavascript = document.createElement("img")
+
+ imagejavascript.src = "https://tse1.mm.bing.net/th?id=OIP.tb6bhXOWM1cUepocTKX2qgHaFm&pid=Api&P=0&h=180"
+
+var imagejavascript = document.createElement("img")
+
+ imagejavascript.src = "https://tse2.mm.bing.net/th?id=OIP.AKMdmQkF6ai2BPC8So_r7QHaEK&pid=Api&P=0&h=180"
+
+var imagejavascript = document.createElement("img")
+
+ imagejavascript.src = "https://tse3.mm.bing.net/th?id=OIP.HcvF8ebcFYuFTv0zYGEf7QHaFE&pid=Api&P=0&h=180"
